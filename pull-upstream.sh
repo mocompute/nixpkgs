b0636d539960947f0ce0e710ab3ac9643c16d335
mo <mocompute@outlook.com>
2025-05-25 13:59:31 +1200

    update for 25.05

--- pull-upstream.sh
+++ pull-upstream.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-git checkout nixos-24.11
-git pull upstream nixos-24.11 --depth 1 --force --rebase
-git reset --hard upstream/nixos-24.11
-git push origin nixos-24.11 --force
+git checkout nixos-25.05
+git pull upstream nixos-25.05 --depth 1 --force --rebase
+git reset --hard upstream/nixos-25.05
+git push origin nixos-25.05 --force
 
-git checkout nixos-24.11-dev
-git pull origin nixos-24.11 --rebase
-git push origin nixos-24.11-dev --force
+git checkout nixos-25.05-dev
+git pull origin nixos-25.05 --rebase
+git push origin nixos-25.05-dev --force
